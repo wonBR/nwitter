ebae6aba4afecf38c11d7a38316002588b958391
wonBR <76773366+wonBR@users.noreply.github.com>
2021-02-02 13:50:12 +0900

    UPDATE log out part

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,13 @@
-import { authService } from "fbase";
 import React from "react";
+import { authService } from "fbase";
+import { useHistory } from "react-router-dom";
 
 export default () => {
-  const onLogOutClick = () => authService.signOut();
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
